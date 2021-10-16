6971def3d3059503603016222c7d3b33e3189371
akan nyong <akanefioknyong@gmail.com>
2021-10-16 15:22:53 +0100

    Another

--- src/nyong/Main.java
+++ src/nyong/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Player player = new Player("Connor", 50, 70, 100);
+        Player player = new Player("Akan", 50, 70, 100);
         player.addEquipment("Car", 2000);
         Game game = new Game(player);
         game.saveGame();
